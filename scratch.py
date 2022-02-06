f4344ec65fa03b472ace3e4d4a0f331dbdf38eb8
Andrew Venoshkin <andvetoshkin@gmail.com>
2022-02-06 16:54:37 +0300

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,16 +1,7 @@
 import re
 
 with open(r'H:\python\projects\yoko_fcs\TIR10601.edf', 'rb') as file:
-    line = str(file.read())
-    pos = line.find('BKCM')
-    print(pos)
-    print(line.find('PVI', pos, pos + 150))
+    line = file.read()
 
-with open(r'H:\python\projects\yoko_fcs\AIRA10101.edf', 'rb') as file:
-    line = str(file.read())
-    print(re.findall(r'[A-Z,_]{2,}', line))
 
-with open(r'H:\python\projects\yoko_fcs\RL_AXENS1.edf', 'rb') as file:
-    line = str(file.read())
-    print(re.findall(r'[A-Z,_]{2,}', line))
 
