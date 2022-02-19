f42caa9d2e2f1ec630aa93ea3edfcbfab1729587
Andrew Venoshkin <andvetoshkin@gmail.com>
2022-02-19 21:36:12 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	scratch.py

--- window.py
+++ window.py
@@ -1,14 +1,17 @@
 import os
 import pathlib
 
-project_path = pathlib.Path.cwd()
-paths_to_GRs = []
+#project_path = pathlib.Path.cwd()
+project_path = 'H:/Yoko/ISOMER/HIS1064/WINDOW'
 
 
-with os.scandir(project_path) as entries_list:
-    for entry in entries_list:
-        if 'EDF' in str(entry):
-            paths_to_GRs.append(str(project_path) + r'/' + entry.name + r'/')
+def get_windows_names(project_path):
+    paths_to_grs = []
+    with os.scandir(project_path) as entries_list:
+        for entry in entries_list:
+            if 'EDF' in str(entry):
+                paths_to_grs.append(str(project_path) + r'/' + entry.name + r'/')
+    return paths_to_grs
 
 
 def change_something(path_to_file, var_1, var_2):
@@ -17,5 +20,6 @@ def change_something(path_to_file, var_1, var_2):
     path.write_text(text, encoding="utf-8-sig")
 
 
-for gr_window in paths_to_GRs:
-    change_something(gr_window + 'main.xaml', 'FixedRatio', 'FreeScale')
+for window_name in get_windows_names(project_path):
+    #change_something(window_name + 'main.xaml', 'FixedRatio', 'FreeScale')
+    print(window_name)
