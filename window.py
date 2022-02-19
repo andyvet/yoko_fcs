886287a3843d3c318f5260ce7c316eac3f553ea8
Andrew Venoshkin <andvetoshkin@gmail.com>
2022-02-19 20:43:28 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	scratch.py

--- window.py
+++ window.py
@@ -1,14 +1,14 @@
 import os
 import pathlib
 
-project_path = r'H:/Yoko/ISOMER/HIS1064/WINDOW/'
+project_path = pathlib.Path.cwd()
 paths_to_GRs = []
 
 
 with os.scandir(project_path) as entries_list:
     for entry in entries_list:
         if 'EDF' in str(entry):
-            paths_to_GRs.append(project_path + entry.name + r'/')
+            paths_to_GRs.append(str(project_path) + r'/' + entry.name + r'/')
 
 
 def change_something(path_to_file, var_1, var_2):
